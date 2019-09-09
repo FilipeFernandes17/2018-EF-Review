0ee94fdb550309dc4be862433e207cf9ec359d6b
FilipeFernandes17 <43007033+FilipeFernandes17@users.noreply.github.com>
2019-09-09 16:48:29 -0600

    1

--- src/EF6/WestWindConsole/DAL/WestWindContext.cs
+++ src/EF6/WestWindConsole/DAL/WestWindContext.cs
@@ -8,12 +8,23 @@ using WestWindConsole.Entities;
 
 namespace WestWindConsole.DAL
 {
+    //My database context class is a "virtual representation"
+    //of the database, with each DbSet<T> property referencing
+    //a particular table in the database
+    //it inherits from the DbContext class.
     public class WestWindContext : DbContext
     {
         public WestWindContext() : base("name=WWdb")
         {
             // TODO: Demonstrate null database initializer
+            //Tell EF that it should NOT create any tables 
+            //in the database on my behalf 
+            //(null == no intitalizer)
+            //This is an example of disabling database
+            //initialization programmatically.
             Database.SetInitializer<WestWindContext>(null);
+            //An alternative to this would be to disable database
+            //intialization in the .configfile
         }
 
         public DbSet<Product> Products { get; set; }
@@ -26,5 +37,7 @@ namespace WestWindConsole.DAL
         public DbSet<Shipment> Shipments { get; set; }
 
         // TODO: Practice - Add entities and DbSet<> properties for the remaining tables
+
+
     }
 }
--- src/EF6/WestWindConsole/Entities/Category.cs
+++ src/EF6/WestWindConsole/Entities/Category.cs
@@ -9,10 +9,10 @@ using System.Threading.Tasks;
 namespace WestWindConsole.Entities
 {
     // TODO: Begin reviewing entity attributes
-    [Table("Categories")]
+    [Table("Categories", Schema = "dbo")]//dbo is default
     public class Category
     {
-        [Key]
+        [Key]// This property/column is a PK in the DB
         public int CategoryID { get; set; }
 
         [Required] // Use this for string/varchar columns that are NOT NULL
@@ -21,12 +21,19 @@ namespace WestWindConsole.Entities
 
         public string Description { get; set; }
 
-        public byte[] Picture { get; set; }
+        public byte[] Picture { get; set; }// image
 
         [StringLength(40, ErrorMessage = "Picture Mime Type has a maximum of 40 characters")]
         public string PictureMimeType { get; set; }
 
         // TODO: Introducing Navigation Properties
+        //the virtual keyword allows our entity to support
+        //"Lazy loading"
+        //The iCollection<T> is an interface data type.
+        //The HashSet<T> is a class that "implements" the 
+        //ICollection<T> interface
+
+        //saying each category can hold many products
         public virtual ICollection<Product> Products { get; set; } =
             new HashSet<Product>();
     }
